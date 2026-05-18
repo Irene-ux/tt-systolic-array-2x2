df71496426f13dcc41f83c55a0b252ec2fd8d50c
Irene-ux <ireneraphael2000@gmail.com>
2026-05-18 12:34:51 +0200

    Add TT wrapper module with serial input loading

--- src/tt_um_systolic_array.v
+++ src/tt_um_systolic_array.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Irene Raphael
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_systolic_array (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,13 +15,42 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+wire start = ui_in[0];
+wire load = ui_in[1];
+wire [1:0] sel = ui_in[3:2];
+wire [3:0] data = ui_in[7:4];
+reg [3:0] a_row0, a_row1, weight_col0, weight_col1;
+wire [7:0] data_out;
+assign uo_out = data_out;
+wire valid1;
+assign uio_out = {7'b0,valid1};
+assign uio_oe = 8'b00000001;
+systolic_array systolic_array_inst(
+  .clk(clk),
+  .rst_n(rst_n),
+  .start(start),
+  .a_row0(a_row0),
+  .a_row1(a_row1),
+  .weight_col0(weight_col0),
+  .weight_col1(weight_col1),
+  .data_out(data_out),
+  .valid(valid1)
+);
+wire _unused = &{ena, uio_in, 1'b0};
+always@(posedge clk)begin
+   if(!rst_n)begin
+    a_row0 <= 0;
+    a_row1 <= 0;
+    weight_col0 <= 0;
+    weight_col1 <= 0;
+   end
+   else if(load) begin
+    case(sel)
+        2'b00: a_row0 <= data;
+        2'b01: a_row1 <= data;
+        2'b10: weight_col0 <= data;
+        2'b11: weight_col1 <= data;
+    endcase
+   end
+end
 endmodule
